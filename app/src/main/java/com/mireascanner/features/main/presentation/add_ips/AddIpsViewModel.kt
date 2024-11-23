73dec7b272f0078edf145af18d0b98305eb797b6
Artyom Rodionov <artemr19032006@gmail.com>
2024-11-23 22:33:39 +0300

    правки

--- app/src/main/java/com/mireascanner/features/main/presentation/add_ips/AddIpsViewModel.kt
+++ app/src/main/java/com/mireascanner/features/main/presentation/add_ips/AddIpsViewModel.kt
@@ -1,6 +1,5 @@
 package com.mireascanner.features.main.presentation.add_ips
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.mireascanner.common.main.domain.models.AddIp
@@ -9,7 +8,6 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
-import kotlin.random.Random
 
 
 @HiltViewModel
@@ -19,22 +17,21 @@ class AddIpsViewModel @Inject constructor() : ViewModel() {
     private val _ipsState = MutableStateFlow(emptyList<AddIp>())
     val ipsState = _ipsState.asStateFlow()
 
+    private var increment = 0
+
 
     fun addIp(ip: String) {
         viewModelScope.launch {
-            Log.d("IP", "here")
-            val id = Random.nextInt(0, 999999999)
+            val id = ++increment
             val addIp = AddIp(id, ip)
             ips[id] = ip
             val list = ipsState.value.toMutableList()
             list.add(addIp)
             _ipsState.value = list.toList()
-            Log.d("IP", ipsState.value.toString())
         }
     }
 
     fun removeAddIp(addIp: AddIp) {
-        Log.d("IP", addIp.id.toString())
         viewModelScope.launch {
             ips.remove(addIp.id)
             val list = ipsState.value.toMutableList()
